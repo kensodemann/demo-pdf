5b4e6b84e1f751b35c901497a443bc680e4e9557
Ken Sodemann <ken@ionic.io>
2021-09-16 06:56:06 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'demo-pdf',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
