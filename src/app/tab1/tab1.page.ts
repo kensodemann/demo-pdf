f1efc746566086e22d3f351ebccc4f05b2fa9cd1
Ken Sodemann <ken@ionic.io>
2021-09-16 14:07:12 -0500

    feat(pdf): show the PDF

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { PdfService } from '../core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(private pdf: PdfService, private toastController: ToastController) {}
+
+  async store() {
+    await this.pdf.storeDocument();
+    this.presentToast();
+  }
+
+  async clear() {
+    await this.pdf.removeDocument();
+    this.presentToast();
+  }
+
+  private async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Requested change has been made.',
+      position: 'top',
+      duration: 2000,
+    });
+    toast.present();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { PdfService } from '../core';
 
 @Component({
   selector: 'app-tab2',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-  constructor() {}
+  @ViewChild('canvas', { static: true }) canvasRef: ElementRef;
+
+  constructor(private pdf: PdfService) {}
+
+  async ionViewDidEnter() {
+    if (await this.pdf.documentExists()) {
+      const doc = await this.pdf.getDocument();
+      this.pdf.renderPage(doc, this.canvasRef.nativeElement, 1);
+    } else {
+      this.erase();
+    }
+  }
+
+  private erase() {
+    const canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
 }
